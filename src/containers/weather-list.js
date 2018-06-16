7bd69e396eae78e11f027cf904d7475017ebb27a
warb1rd <mahima.m23@gmail.com>
2018-06-15 17:02:23 -0700

    google map integration

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart.js'; 
+import GoogleMap from '../components/google-map.js';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+
     const name = cityData.city.name;
-    // const temp = cityData.list.map(weather => weather.main.temp)
-    // console.log(name)
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lat, lon } = cityData.city.coord;
+
     return (
       <tr key={name}>
-        <td>{name}</td>
-        {/* <td>{temp}</td> */}
+        <td><GoogleMap lat={lat} lon={lon} /></td>
+        <td><Chart data={temps} color="orange" units="F" /></td>
+        <td><Chart data={pressures} color="green" units="hpa"/></td>
+        <td><Chart data={humidities} color="blue" units="%"/></td>
       </tr>
     )
   }
@@ -21,9 +29,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (F)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>    
         </thead>
         <tbody>
